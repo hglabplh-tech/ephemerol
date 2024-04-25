c30614f71c363352db9496ac1b78fb6857052a5d
Harald Glab-Plhak <harald.glab-plhak@t-online.de>
2024-04-26 00:39:49 +0200

    lifted versions oj clojure.core and active-clojure

--- project.clj
+++ project.clj
@@ -3,5 +3,5 @@
   :url "http://github.com/active-group/ephemerol"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]
-                 [active-clojure "0.3.0" :exclusions [org.clojure/clojure]]])
+  :dependencies [[org.clojure/clojure "1.11.1"]
+                 [de.active-group/active-clojure "0.42.2" :exclusions [org.clojure/clojure]]])
